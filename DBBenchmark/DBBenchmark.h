2d5a0432e2c516bf8f3c3c20bbe62b27693b13a1
Dima Bart <dima.bart01@gmail.com>
2015-03-15 20:53:58 -0400

    Adds license and cleans up the header

--- DBBenchmark/DBBenchmark.h
+++ DBBenchmark/DBBenchmark.h
@@ -1,9 +1,32 @@
 //
 //  DBBenchmark.h
 //
-//  Created by Dima Bart on 2013-09-09.
-//  Copyright (c) 2013 Dima Bart. All rights reserved.
+//  Copyright (c) 2015 Dima Bart
+//  All rights reserved.
 //
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this
+//  list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation
+//  and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+//  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//  The views and conclusions contained in the software and documentation are those
+//  of the authors and should not be interpreted as representing official policies,
+//  either expressed or implied, of the FreeBSD Project.
 
 #import <Foundation/Foundation.h>
 
@@ -21,9 +44,9 @@ typedef void (^DBBenchmarkBlock)(void);
 @end
 
 #ifdef __cplusplus
-#define DB_EXTERN extern "C"
+#define DB_EXTERN extern "C" __attribute__((always_inline))
 #else
-#define DB_EXTERN extern
+#define DB_EXTERN extern __attribute__((always_inline))
 #endif
 
 #if !DEBUG
@@ -35,9 +58,9 @@ typedef void (^DBBenchmarkBlock)(void);
 
 #else
 
-DB_EXTERN void DBBenchmarkWithBlock(NSString *name, DBBenchmarkBlock block) __attribute__((always_inline));
-DB_EXTERN void DBBenchmarkDefault(DBBenchmarkBlock block) __attribute__((always_inline));
-DB_EXTERN void DBBenchmarkStart() __attribute__((always_inline));
-DB_EXTERN void DBBenchmarkEnd(NSString *name, ...) __attribute__((always_inline));
+DB_EXTERN void DBBenchmarkWithBlock(NSString *name, DBBenchmarkBlock block);
+DB_EXTERN void DBBenchmarkDefault(DBBenchmarkBlock block);
+DB_EXTERN void DBBenchmarkStart();
+DB_EXTERN void DBBenchmarkEnd(NSString *name, ...);
 
 #endif
